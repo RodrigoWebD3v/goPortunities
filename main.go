4fff1bb410b4d67a4c69ef9add786db11f172431
Rodrigo Cardoso <92650721+RodrigoWebD3v@users.noreply.github.com>
2025-03-16 22:24:14 -0300

    Add endpoints
    Add endpoints initials

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RodrigoWebD3v/goPortunities/router"
+)
+
+
+func init(){
+	fmt.Printf("Initializing server...")
+}
+
+
+func main() {
+	router.Initialize()
+}
